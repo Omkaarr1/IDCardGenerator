1737a5c2cd640b6a9bbcdcea98c0e1bc114a6f3a
OMKAR <omkargupta702@gmail.com>
2024-07-24 11:32:44 +0530

    -update

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def download_file():
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000,debug=True)
